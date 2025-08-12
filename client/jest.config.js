ce733705a51358c6921697bf83b005bdba40a9f4
mayma288 <oumaimajl88@gmail.com>
2025-08-12 20:00:53 +0100

    test frontend

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   testEnvironment: 'jsdom', //Simulates a browser environment so your React components can render as if in a real browser.
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],//Makes Jest understand imports of CSS or style files, which Next.js uses, but Jest doesn't natively understand. This avoids errors when your components import CSS.
+setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],//Makes Jest understand imports of CSS or style files, which Next.js uses, but Jest doesn't natively understand. This avoids errors when your components import CSS.
 
 
-  moduleNameMapper: {//Makes Jest understand imports of CSS or style files, which Next.js uses, but Jest doesn't natively understand. This avoids errors when your components import CSS.
+moduleNameMapper: {//Makes Jest understand imports of CSS or style files, which Next.js uses, but Jest doesn't natively understand. This avoids errors when your components import CSS.
 
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
+},
+transform: {
+'^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+}
 };
